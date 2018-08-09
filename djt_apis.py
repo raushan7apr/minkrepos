f135aad326e577310e7d1c738078778ba21efdb2
RAUSHAN KUMAR <raushankumar@RAUSHANs-MacBook-Pro.local>
2018-08-09 13:04:10 +0530

    Phase1 Requirement of DJT has been completed

--- djt_apis.py
+++ djt_apis.py
@@ -2,20 +2,27 @@ import requests
 import json
 import csv
 
-BASE_URL = "https://madrid.b1cloud.in:50000/b1s/v1/"
-company_db = "DJTRETAILERSTEST"
+# BASE_URL = "https://madrid.b1cloud.in:50000/b1s/v1/"
+# company_db = "DJTRETAILERSTEST"
+BASE_URL = "https://oklahoma.b1cloud.in:50000/b1s/v1/"
+company_db = "DJTRETAILERSTESTONE"
 user_name = "b1cloud\\nav.user01"
 password = "India***2018"
 
 session_id = ""
 
 barcode_details_list = []
+batch_details_list = {}
 product_details_list = []
 product_price_details_list = []
 taxcode_rate_map = {}
+item_store_map = {}
+item_batch_map = {}
+warehouse_pricecode_map={}
+storemap={"01":"998","STR01":"999"}
 
 def login():
-	payload = "{\"CompanyDB\":\"DJTRETAILERSTEST\",\"UserName\":\"b1cloud\\\\nav.user01\",\"Password\":\"India***2018\"\r\n}"
+	payload = "{\"CompanyDB\":\"DJTRETAILERSTESTONE\",\"UserName\":\"b1cloud\\\\nav.user01\",\"Password\":\"India***2018\"\r\n}"
 	resp = requests.post(BASE_URL+"Login", headers={"Content-Type":"application/json"}, data=payload)
 	# print payload
 	#data={"CompanyDB":"DJTRETAILERSTEST","UserName":"b1cloud\\nav.user01","Password":"India***2018"}
@@ -29,9 +36,9 @@ def login():
 def populate_tax_code(item):
 	tax_code = str(item["U_TaxCode"])
 	item_code = str(item["ItemCode"].encode('utf-8'))
-	print "taxcode_rate_map : " + str(taxcode_rate_map)
-	print "tax_code : " + tax_code
-	print "itemCode : " + item_code
+	print("taxcode_rate_map  " + str(taxcode_rate_map))
+	print("tax_code  " + tax_code)
+	print("itemCode  " + item_code)
 	if tax_code == "None":
 		return
 	if tax_code in taxcode_rate_map:
@@ -43,9 +50,9 @@ def populate_tax_code(item):
 
 def fetch_item_details(items_list):
 	for item in items_list:
-		# print item["ItemCode"]
 		product_details = {}
-		product_details["sku"] = str(item["ItemCode"].encode('utf-8'))
+		itemCode = str(item["ItemCode"].encode('utf-8'))
+		product_details["sku"] = itemCode
 		try:
 			product_details["productName"] = str(item["ItemName"].encode('utf-8')).replace(',','_')
 		except:
@@ -66,18 +73,45 @@ def fetch_item_details(items_list):
 		product_details["hsn"] = str(item["ChapterID"])
 		product_details_list.append(product_details)
 
-		product_price_details = {}
-		product_price_details["sku"] = str(item["ItemCode"].encode('utf-8'))
-		price_list = item["ItemPrices"]
+		#To save the respective store of sku
+		store_list  = item["ItemWarehouseInfoCollection"]
+		for store in store_list:
+			if itemCode in item_store_map:
+				item_store_map[itemCode].append(store["WarehouseCode"])
+			else:
+				item_store_map[itemCode] = []
+				item_store_map[itemCode].append(store["WarehouseCode"])
 
-		for price in price_list:
-			if price["PriceList"] == 1:
-				product_price_details["productMRP"] = price["Price"]
-			elif price["PriceList"] == 2:
-				product_price_details["shopPrice"] = price["Price"]
-		product_price_details["isLatestPrice"] = "Y"
-		product_price_details_list.append(product_price_details)
-		populate_tax_code(item)
+			product_price_details = {}
+			product_price_details["sku"] = str(item["ItemCode"].encode('utf-8'))
+			price_list = item["ItemPrices"]
+			product_price_details["productMRP"] = None
+			product_price_details["shopPrice"] = None
+			product_price_details["isLatestPrice"] = "Y"
+			product_price_details["warehouseCode"] = store["WarehouseCode"]
+
+			for price in price_list:
+				if str(price["PriceList"]) == str(warehouse_pricecode_map[store["WarehouseCode"]]["MRP_CODE"]):
+					product_price_details["productMRP"] = price["Price"]
+				elif str(price["PriceList"]) == str(warehouse_pricecode_map[store["WarehouseCode"]]["CSP_CODE"]):
+					product_price_details["shopPrice"] = price["Price"]
+
+			if str(item["ManageBatchNumbers"])=="tYES":
+				batchList = batch_details_list.get(itemCode)
+				if batchList == None:
+					product_price_details_list.append(product_price_details)
+				else:
+					for batch in batchList:
+						temp_product_price_details = {}
+						temp_product_price_details["sku"]=product_price_details["sku"]
+						temp_product_price_details["shopPrice"]=product_price_details["shopPrice"]
+						temp_product_price_details["isLatestPrice"] = "Y"
+						temp_product_price_details["warehouseCode"] = product_price_details["warehouseCode"]
+						temp_product_price_details["productMRP"]=batch["BatchMRP"]
+						product_price_details_list.append(temp_product_price_details)
+			else:
+				product_price_details_list.append(product_price_details)
+			populate_tax_code(item)
 
 def fetch_barcode_details(barcodes_list):
 	for barcode in barcodes_list:
@@ -91,6 +125,8 @@ def get_items():
 	resp = login()
 	session_id = resp.json()['SessionId']
 	headers={"B1SESSION": str(session_id),"ROUTEID": ".node0","Content-Type": "application/json"}
+	get_warehouse_details()
+	get_batch_numbers()
 	itemApiURL = "Items"
 	nextPage = True;
 	while nextPage:
@@ -99,9 +135,7 @@ def get_items():
 		items_list = response.json()["value"]
 		if 'odata.nextLink' in data:
 			nextLink=response.json()["odata.nextLink"]
-			print(nextLink)
 			itemApiURL = "Items" + nextLink[nextLink.find('?'):]
-			print(itemApiURL)
 		else:
 			nextPage=None
 		fetch_item_details(items_list)
@@ -115,26 +149,78 @@ def get_barcodes():
 		barcodes_list = response.json()["value"]
 		if 'odata.nextLink' in data:
 			nextLink=response.json()["odata.nextLink"]
-			print(nextLink)
 			barcodeApiURL = "BarCodes" + nextLink[nextLink.find('?'):]
 		else:
 			nextPage=None
 		fetch_barcode_details(barcodes_list)
 
+def get_batch_numbers():
+	batchNumberApiURL="BatchNumberDetails"
+	nextPage=True
+	while nextPage:
+		response = requests.get(BASE_URL+batchNumberApiURL,cookies=resp.cookies)
+		data=response.json()
+		batch_list = response.json()["value"]
+		if 'odata.nextLink' in data:
+			nextLink=response.json()["odata.nextLink"]
+			batchNumberApiURL = "BatchNumberDetails" + nextLink[nextLink.find('?'):]
+		else:
+			nextPage=None
+	fetch_batch_numbers(batch_list)
+
+def fetch_batch_numbers(batch_list):
+	for batch in batch_list:
+		itemCode = batch["ItemCode"]
+		batch_details = {}
+		batch_details["BatchMRP"] = batch["U_BatchMRP"]
+		batch_details["Batch"] = batch["Batch"]
+		if itemCode in batch_details_list:
+			batch_details_list[itemCode].append(batch_details)
+		else:
+			batch_details_list[itemCode] = []
+			batch_details_list[itemCode].append(batch_details)
+
+def get_warehouse_details():
+	warehouseApiURL="Warehouses"
+	nextPage = True
+	while nextPage:
+		response = requests.get(BASE_URL + warehouseApiURL, cookies=resp.cookies)
+		data = response.json()
+		warehouse_list = response.json()["value"]
+		if 'odata.nextLink' in data:
+			nextLink = response.json()["odata.nextLink"]
+			warehouseApiURL = "BatchNumberDetails" + nextLink[nextLink.find('?'):]
+		else:
+			nextPage = None
+	fetch_warehouse_details(warehouse_list)
+
+def fetch_warehouse_details(warehouse_list):
+	for warehouse in warehouse_list:
+		warehouseCode = warehouse["WarehouseCode"]
+		warehouseDetails = {}
+		warehouseDetails["CSP_CODE"]=warehouse["U_CSP_PriceList"]
+		warehouseDetails["MRP_CODE"]=warehouse["U_MRP_PriceList"]
+		warehouse_pricecode_map[warehouseCode]=warehouseDetails
+
 def write_to_csv():
 	with open('D:\DJT\scripts\\bd.csv', 'w') as bd:
 		for barcode_details in barcode_details_list:
-			bd.write(barcode_details["barcodeId"]+","+barcode_details["sku"])
-			bd.write('\n')
+			storeList = item_store_map.get(barcode_details["sku"])
+			if storeList != None:
+				for store in storeList :
+					bd.write(barcode_details["barcodeId"]+","+barcode_details["sku"]+","+storemap[store])
+					bd.write('\n')
 		bd.close()
 	with open('D:\DJT\scripts\pd.csv', 'w') as pd:
 		for product_details in product_details_list:
-			pd.write(product_details["sku"]+","+product_details["productName"]+","+product_details["productDescription"]+","+product_details["hsn"]+","+product_details["weightFlag"])
-			pd.write('\n')
+			storeList = item_store_map[product_details["sku"]]
+			for store in storeList:
+				pd.write(product_details["sku"]+","+product_details["productName"]+","+product_details["productDescription"]+","+product_details["hsn"]+","+product_details["weightFlag"]+","+storemap[store])
+				pd.write('\n')
 		pd.close()
 	with open('D:\DJT\scripts\ppd.csv', 'w') as ppd:
 		for product_price_details in product_price_details_list:
-			ppd.write(product_price_details["sku"]+","+str(product_price_details["shopPrice"])+","+str(product_price_details["productMRP"])+","+product_price_details["isLatestPrice"])
+			ppd.write(product_price_details["sku"]+","+str(product_price_details["shopPrice"])+","+str(product_price_details["productMRP"])+","+product_price_details["isLatestPrice"]+","+storemap[product_price_details["warehouseCode"]])
 			ppd.write('\n')
 		ppd.close()
 	with open('D:\DJT\scripts\sku_tax_rel.csv', 'w') as sku_tax_rel:
@@ -156,7 +242,12 @@ if __name__ == "__main__" :
 	#login()
 	get_items()
 	get_barcodes()
-	# print barcode_details_list
-	# print product_price_details_list
-	print taxcode_rate_map
+	#print("Printing item store map .... \n")
+	#print(item_store_map)
+	# for barcode_details in barcode_details_list:
+	# 	print(item_store_map.get(barcode_details["sku"]))
+	# 	for store in storeList:
+	# 		bd.write(barcode_details["barcodeId"] + "," + barcode_details["sku"] + "," + storemap[store])
+	# 		bd.write('\n')
+	# print(item_store_map[barcode_details["400001"]])
 	write_to_csv()
